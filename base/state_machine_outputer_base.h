0cd84dff4177e93386cd7a85dc692d9f57c21c09
guanzhenlin <942940422@qq.com>
2026-07-23 11:27:28 +0800

    optimize

--- base/state_machine_outputer_base.h
+++ base/state_machine_outputer_base.h
@@ -5,15 +5,20 @@
 #include "state_machine_switcher_base.h"
 
 #include <memory>
+#include <map>
+#include <functional>
 
-template <typename Data, typename Param, typename Inputer, typename Switcher,
+template <typename State, typename Param, typename Inputer, typename Switcher,
 typename = typename std::enable_if_t<std::is_base_of_v<StateMachineParamBase, Param>>,
 typename = typename std::enable_if_t<std::is_base_of_v<StateMachineInputerBase<Param>, Inputer>>,
-typename = typename std::enable_if_t<std::is_base_of_v<StateMachineSwitcherBase<Data, Param, Inputer>, Switcher>>>
+typename = typename std::enable_if_t<std::is_base_of_v<StateMachineSwitcherBase<State, Param, Inputer>, Switcher>>>
 class StateMachineOutputerBase
 {
+public: 
+    using ActionTable = std::map<State, std::function<void(std::shared_ptr<Param>, std::shared_ptr<Inputer>, std::shared_ptr<Switcher>)>>;
 private:
     std::shared_ptr<StateMachineActionBase> action_sptr_{nullptr};
+    ActionTable action_table_;
 public:
     StateMachineOutputerBase(std::shared_ptr<StateMachineActionBase> action = std::make_shared<StateMachineActionBase>()) : 
     action_sptr_(action)
@@ -23,8 +28,32 @@ public:
     void Init()
     {
         InitWriters();
+        InitActionTable();
     }
 public:
     virtual void InitWriters() = 0;
-    virtual void UpdateAction(std::shared_ptr<Param> param, std::shared_ptr<Inputer> input, std::shared_ptr<Switcher> switcher) = 0;
+    virtual void InitActionTable() = 0;
+public:
+    void UpdateAction(std::shared_ptr<Param> param, std::shared_ptr<Inputer> input, std::shared_ptr<Switcher> switcher)
+    {
+        auto crnt_state = switcher->GetCrntState();
+        auto iter = action_table_.find(crnt_state);
+        if (iter != action_table_.end())
+        {
+            iter->second(param, input, switcher);
+        }
+    }
+protected:
+    void SetActionTable(const std::map<State, std::function<void(std::shared_ptr<Param>, std::shared_ptr<Inputer>, std::shared_ptr<Switcher>)>>& table)
+    {
+        action_table_ = table;
+    }
+    void AddAction(State state, std::function<void(std::shared_ptr<Param>, std::shared_ptr<Inputer>, std::shared_ptr<Switcher>)> func)
+    {
+        action_table_[state] = func;
+    }
+    const ActionTable & GetActionTable() const noexcept
+    {
+        return action_table_;
+    }
 };
--- parking/avp/avp_cruising_outputer.h
+++ parking/avp/avp_cruising_outputer.h
@@ -20,9 +20,69 @@ public:
     {
         std::cout << "Init AvpCruisingOutputer Writers" << std::endl;
     };
-    void UpdateAction(std::shared_ptr<AvpCruisingParam> param, std::shared_ptr<AvpCruisingInputer> input, std::shared_ptr<AvpCruisingStateSwitcher> switcher) override
+    void InitActionTable() override 
     {
-        std::cout << "Update AvpCruisingOutputer Action" << std::endl;
+        AddAction(AvpCruisingStateType::IDLE_0, std::bind(&AvpCruisingOutputer::ActionIdle, this, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3));
+        AddAction(AvpCruisingStateType::STANDBY_1, std::bind(&AvpCruisingOutputer::ActionStandby, this, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3));
+        AddAction(AvpCruisingStateType::LOCATING_2, std::bind(&AvpCruisingOutputer::ActionLocating, this, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3));
+        AddAction(AvpCruisingStateType::LOCATED_3, std::bind(&AvpCruisingOutputer::ActionLocated, this, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3));
+        AddAction(AvpCruisingStateType::PREPARED_4, std::bind(&AvpCruisingOutputer::ActionPrepared, this, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3));
+        AddAction(AvpCruisingStateType::CRUISING_5, std::bind(&AvpCruisingOutputer::ActionCruising, this, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3));
+        AddAction(AvpCruisingStateType::PARKING_6, std::bind(&AvpCruisingOutputer::ActionParking, this, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3));
+        AddAction(AvpCruisingStateType::OVERRIDE_7, std::bind(&AvpCruisingOutputer::ActionOverride, this, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3));
+        AddAction(AvpCruisingStateType::SUCCESS_8, std::bind(&AvpCruisingOutputer::ActionSuccess, this, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3));
+        AddAction(AvpCruisingStateType::FAILED_9, std::bind(&AvpCruisingOutputer::ActionFailed, this, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3));
+        AddAction(AvpCruisingStateType::SUSPEND_10, std::bind(&AvpCruisingOutputer::ActionSuspend, this, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3));
+        AddAction(AvpCruisingStateType::TERMINATE_11, std::bind(&AvpCruisingOutputer::ActionTerminate, this, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3));
+    };
+private:
+    void ActionIdle(std::shared_ptr<AvpCruisingParam> param, std::shared_ptr<AvpCruisingInputer> input, std::shared_ptr<AvpCruisingStateSwitcher> switcher)
+    {
+        std::cout << "AvpCruisingOutputer Action : IDLE" << std::endl;
+    };
+    void ActionStandby(std::shared_ptr<AvpCruisingParam> param, std::shared_ptr<AvpCruisingInputer> input, std::shared_ptr<AvpCruisingStateSwitcher> switcher)
+    {
+        std::cout << "AvpCruisingOutputer Action : STANDBY" << std::endl;
+    };
+    void ActionLocating(std::shared_ptr<AvpCruisingParam> param, std::shared_ptr<AvpCruisingInputer> input, std::shared_ptr<AvpCruisingStateSwitcher> switcher)
+    {
+        std::cout << "AvpCruisingOutputer Action : LOCATING" << std::endl;
+    };
+    void ActionLocated(std::shared_ptr<AvpCruisingParam> param, std::shared_ptr<AvpCruisingInputer> input, std::shared_ptr<AvpCruisingStateSwitcher> switcher)
+    {
+        std::cout << "AvpCruisingOutputer Action : LOCATED" << std::endl;
+    };
+    void ActionPrepared(std::shared_ptr<AvpCruisingParam> param, std::shared_ptr<AvpCruisingInputer> input, std::shared_ptr<AvpCruisingStateSwitcher> switcher)
+    {
+        std::cout << "AvpCruisingOutputer Action : PREPARED" << std::endl;
+    };
+    void ActionCruising(std::shared_ptr<AvpCruisingParam> param, std::shared_ptr<AvpCruisingInputer> input, std::shared_ptr<AvpCruisingStateSwitcher> switcher)
+    {
+        std::cout << "AvpCruisingOutputer Action : CRUISING" << std::endl;
+    };
+    void ActionParking(std::shared_ptr<AvpCruisingParam> param, std::shared_ptr<AvpCruisingInputer> input, std::shared_ptr<AvpCruisingStateSwitcher> switcher)
+    {
+        std::cout << "AvpCruisingOutputer Action : PARKING" << std::endl;
+    };
+    void ActionOverride(std::shared_ptr<AvpCruisingParam> param, std::shared_ptr<AvpCruisingInputer> input, std::shared_ptr<AvpCruisingStateSwitcher> switcher)
+    {
+        std::cout << "AvpCruisingOutputer Action : OVERRIDE" << std::endl;
+    };
+    void ActionSuccess(std::shared_ptr<AvpCruisingParam> param, std::shared_ptr<AvpCruisingInputer> input, std::shared_ptr<AvpCruisingStateSwitcher> switcher)
+    {
+        std::cout << "AvpCruisingOutputer Action : SUCCESS" << std::endl;
+    };
+    void ActionFailed(std::shared_ptr<AvpCruisingParam> param, std::shared_ptr<AvpCruisingInputer> input, std::shared_ptr<AvpCruisingStateSwitcher> switcher)
+    {
+        std::cout << "AvpCruisingOutputer Action : FAILED" << std::endl;
+    };
+    void ActionSuspend(std::shared_ptr<AvpCruisingParam> param, std::shared_ptr<AvpCruisingInputer> input, std::shared_ptr<AvpCruisingStateSwitcher> switcher)
+    {
+        std::cout << "AvpCruisingOutputer Action : SUSPEND" << std::endl;
+    };
+    void ActionTerminate(std::shared_ptr<AvpCruisingParam> param, std::shared_ptr<AvpCruisingInputer> input, std::shared_ptr<AvpCruisingStateSwitcher> switcher)
+    {
+        std::cout << "AvpCruisingOutputer Action : TERMINATE" << std::endl;
     };
 };
 };
